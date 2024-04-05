ebe7c2062aae2ce3fa0908ad81310940fd46d5c7
jieun615 <je060105@gmail.com>
2024-04-05 23:49:57 +0900

    user entity 데코레이터 수정

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -12,20 +12,24 @@ import {
 import { Chat } from './chat.entity';
 
 import * as bcrypt from 'bcrypt';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Length, Matches } from 'class-validator';
 
-@Entity()
+@Entity({ name: 'user' })
 export class User extends BaseEntity {
   @PrimaryColumn({ type: 'varchar' })
   @IsNotEmpty()
   id: string;
 
   @Column()
+  @Length(8, 20)
   @IsNotEmpty()
   password: string;
 
   @Column()
   @IsNotEmpty()
+  @Matches(/\d{2,3}-\d{3,4}-\d{4}/, {
+    message: '전화번호 형식이 올바르지 않습니다.',
+  })
   phoneNumber: string;
 
   @Column()
